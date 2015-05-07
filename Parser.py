587435b4d36bddcf994d59b6f424b1e289b21053
Hitesh Ramani <hiteshramani@hotmail.com>
2015-05-08 00:06:05 +0530

    Updated it for multiple concurrent queries, added comments

--- Parser.py
+++ Parser.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 import os
 import sys
-#f = open("/tmp/test.txt","w")
-os.system("id > /tmp/id.out")
 
-q_String = sys.argv[1]
-q_String = (q_String.upper())
+q_String = sys.argv[1] # Copying arguements in q_String
+q_String = (q_String.upper()) # Converting to Uppercase
 q_O = "ORDER BY"
+
+pid_Process = open('/tmp/pid_Process.txt','r')
+PID = pid_Process.read().rstrip()
+
+# Copy query to the first line of PID.txt and direction to the secondline of PID.txt otherwise leave the file blank(if there is no order by command)
 if q_O in q_String:
 	q_String_beg = q_String[0:q_String.find(q_O)].strip()
 	if "ASC" in q_String[q_String.find(q_O)+8:].strip():
@@ -15,10 +18,9 @@ if q_O in q_String:
 		q_String_dir = "DESC"
 	else:
 		q_String_dir = "ASC"
-	#/home/hitesh/Desktop/Project/Project_Final_Work
-	parsed_File = open('/tmp/Parsed_Query.txt','w')
+	parsed_File = open('/tmp/'+str(PID)+'.txt','w')
 	parsed_File.write(q_String_beg+'\n')
 	parsed_File.write(q_String_dir)
 else:
-	parsed_File = open('/tmp/Parsed_Query.txt','w')
+	parsed_File = open('/tmp/'+str(PID)+'.txt','w')
 	parsed_File.write('')
